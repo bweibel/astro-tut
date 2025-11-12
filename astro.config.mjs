c02320b1eedab3dab856e43945fef4fb743d7047
Ben Weibel <weibel.b@gmail.com>
2025-11-11 18:58:48 -0800

    Add react and a dynamic greeting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://zesty-kangaroo-271f70.netlify.app/"
+  site: "https://zesty-kangaroo-271f70.netlify.app/",
+  integrations: [preact()]
 });
