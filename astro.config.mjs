7fc7a31c2b6b333c7fdc0015f92204b7f64115d2
Ben Weibel <weibel.b@gmail.com>
2025-11-11 18:44:38 -0800

    Add rss component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://zesty-kangaroo-271f70.netlify.app/"
+});
